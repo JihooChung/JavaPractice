729f292134e2e4cfa00af607feebb7879a05ac76
JihooChung <jihoo1024@ewhain.net>
2022-08-13 02:58:49 +0900

    Chap1

---
+++ chap1_practice/src/chap1_practice/MyInfoApp.java
@@ -0,0 +1,11 @@
+package chap1_practice;
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+		System.out.println("Jihoo Chung");
+		System.out.println("22 years old");
+		System.out.println("Departmant or Computer Engineering");
+	}
+
+}
